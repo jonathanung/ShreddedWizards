bf57f1eab9c0a4b87c43e6bed20536c95e395040
surrealcloud <zepifycontact@gmail.com>
2024-05-19 00:16:42 -0700

    Previous commit broke changes made in commit 0e9d22f - Reconciled changes from previous commits to changes made in 0e9d22f - Includes new variables for MaxHealth and MaxMana, changing items so they use 0e9d22f variable names instead
    Other changes
    - Updated speed and gravity so player is not flying through arenas

--- player.gd
+++ player.gd
@@ -1,14 +1,20 @@
 class_name Player extends CharacterBody2D
 
-var Health: float = 100
+var MaxHealth: int = 100;
 var CurrentHealth: float = 100
-var Mana: int = 10
+
+var Muscle: int = 50;
+
+var MaxMana: int = 10;
 var CurrentMana: float = 10
 
-var Muscle: float = 50
-var Gravity: float = 400
-var JumpForce: float = -1200
-var Velocity: Vector2 = Vector2.ZERO
+var Gravity: int = 250
+
+var JumpVelocity: int = -250
+
+var MaxVelocityX: int = 250
+
+var AccelerationX: int = 100
 
 var item_Base: Array
 var item_Ult: String
@@ -17,53 +23,53 @@ func _init():
 	pass	
 
 func Die():
-	self.Health = 0
+	CurrentHealth = 0
 
 func Damage(amount: int): 
-	self.Health -= amount - self.Muscle * 3
-	if (self.Health < 0):
+	CurrentHealth -= amount - self.Muscle * 0.5
+	if (CurrentHealth <= 0):
 		self.Die()
 
 func UseMana(mana: int):
-	self.Mana -= mana
+	CurrentMana -= mana
 	
-	if (self.Mana < 0):
-		self.Mana = 0
+	if (CurrentMana < 0):
+		CurrentMana = 0
 
 func BaseItemUsed(item: String):
 	if (item == "Gloves"):
-		CurrentHealth = min(CurrentHealth + 0.25 * Health, Health)
+		CurrentHealth = min(CurrentHealth + 0.25 * MaxHealth, MaxHealth)
 	if (item == "Book"):
-		CurrentHealth = min(CurrentHealth + 0.0666 * Health, Health)
-		CurrentMana = min(CurrentMana + 0.0666 * Mana, Mana)
+		CurrentHealth = min(CurrentHealth + 0.0666 * MaxHealth, MaxHealth)
+		CurrentMana = min(CurrentMana + 0.0666 * MaxMana, MaxMana)
 		Muscle *= 1.0666
-		JumpForce *= 1.0666
-		Velocity *= 1.0666
+		JumpVelocity *= 1.0666
+		MaxVelocityX *= 1.0666
 	if (item == "Creatine"):
-		CurrentMana = min(CurrentMana + 0.25 * Mana, Mana)
+		CurrentMana = min(CurrentMana + 0.25 * MaxMana, MaxMana)
 	if (item == "LiftingStraps"):
-		CurrentMana = min(CurrentMana + 0.1 * Mana, Mana)
+		CurrentMana = min(CurrentMana + 0.1 * MaxMana, MaxMana)
 		Muscle *= 1.1
 	if (item == "PreWorkout"):
-		Velocity *= 1.25
+		MaxVelocityX *= 1.25
 	if (item == "ProteinPowder"):
 		Muscle *= 1.25
 
 func UltItemUsed(item: String):
 	if (item == "Roids"):
 		Muscle *= 1.5
-		Velocity *= 1.5
+		MaxVelocityX *= 1.5
 	if (item == "Baton"):
 		#implement extra range later
 		pass
 	if (item == "HealthPot"):
 		#implement current health later
-		CurrentHealth = min(CurrentHealth + 0.5 * Health, Health)
+		CurrentHealth = min(CurrentHealth + 0.5 * MaxHealth, MaxHealth)
 	if (item == "ManaPot"):
-		CurrentMana = Mana
+		CurrentMana = MaxMana
 	if (item == "IceCream"):
 		#implement Marvin Mode later
-		Velocity *= 2.5
+		MaxVelocityX *= 2.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -73,22 +79,19 @@ func _ready():
 	BaseItemUsed(item_Base[1])
 	pass
 
-func _physics_process(delta):		
-	if (not is_on_floor()):
-		velocity.y = Gravity * 50 * delta
+func _physics_process(delta):
+
+	if not is_on_floor():
+		velocity.y += Gravity*delta
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JumpVelocity
+		
+	var direction = Input.get_axis("move_left","move_right")
+	if direction:
+		velocity.x = move_toward(velocity.x,MaxVelocityX*direction,AccelerationX)
 	else:
-		velocity.y = 0
-
-	if (Input.is_action_pressed("move_left")):
-
-		self.velocity.x -= 1000 * delta
-
-	elif (Input.is_action_pressed("move_right")):
-		velocity.x += 1000 * delta
-
-	elif (Input.is_action_pressed("jump")):
-		while (is_on_floor()):
-			velocity.y = JumpForce
+		velocity.x = move_toward(velocity.x,0,AccelerationX)
 		
 	self.move_and_slide()
 
